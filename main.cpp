c7115f6a8c32cd02357da336e38adac48ac0c28d
ColinSeger <112935014+ColinSeger@users.noreply.github.com>
2025-10-27 16:36:27 +0100

    Should work on linux (Cope)

--- main.cpp
+++ main.cpp
@@ -1,38 +1,14 @@
-// #include "external/vulkan/vulkan.h"
 #include <iostream>
-// #include <glm/glm.hpp>
-// #define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-// #include "external/glfw-3.4/include/GLFW/glfw3.h"
-#include <cassert>
+#include <glm/glm.hpp>
+#include "src/application.cpp"
 
 int main()
-{
-    
-    assert(glfwInit() == true && "GLFW Failed to open");
-
-    /*
-    if(!glfwInit()){
-        return 1;
-    }
-    */
-    /**/
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-    
+{   
+    glm::vec2 vector2 = {1, 0};
+    run();
     std::string result;
-    std::cout << "Hello World \n";
+    std::cout << "Hello World \n" << vector2.x;
     std::cin >> result;
-    // glm::vec2 vector2;
     
     return 0;
 }
---
+++ src/application.cpp
@@ -0,0 +1,19 @@
+#include "application.h"
+
+
+void run()
+{
+    assert(glfwInit() == true && "GLFW Failed to open");
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+}
